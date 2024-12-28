0de9598cd326accaf185130754919fc6c78b5057
Sanar <san-ar@users.noreply.github.com>
2024-12-28 14:16:06 +0800

    Add default(black) hover effect to cells

--- javascript.js
+++ javascript.js
@@ -1,9 +1,5 @@
 const grid = document.querySelector(".grid");
 
-grid.addEventListener("mouseenter", (e) => {
-    e.target.style.background = "black";
-});
-
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.classList.add('row');
@@ -12,27 +8,14 @@ for (let i = 0; i < 16; i++) {
         const cell = document.createElement("div");
         cell.classList.add('cell');
         row.appendChild(cell);
+
+        addHoverEffect(cell);
     }
 }
 
-// const cell = document.querySelectorAll(".cell");
 
-// cell.addEventListener("mouseenter", (e) => {
-//     e.target.style.background  = "black";
-// });
-
-// function addHoverEffect (item) {
-//     item.addEventListener("mouseenter", (e) => {
-//         e.target.style.background = "black";
-//     });
-// }
-
-const cell =  document.querySelectorAll(".cell");
-
-document.addEventListener("DOMContentLoaded", () => {
-   
-
-   cell.addEventListener("mouseenter", (e) => {
+function addHoverEffect(item) {
+    item.addEventListener("mouseenter", (e) => {
         e.target.style.background = "black";
-   });
-});
+    });
+}
