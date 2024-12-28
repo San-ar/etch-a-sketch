498436d50e3f3fee56bbe310a426fdd0f0a47dbd
Sanar <san-ar@users.noreply.github.com>
2024-12-28 16:30:16 +0800

    Add button to adjust grid size according to user input

--- javascript.js
+++ javascript.js
@@ -1,21 +1,46 @@
+let gridSize = 16;
+
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add('row');
-    grid.appendChild(row);
-    for (let i = 0; i < 16; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add('cell');
-        row.appendChild(cell);
+const gridSizeBtn = document.querySelector(".grid-button");
 
-        addHoverEffect(cell);
+createGrid(gridSize);
+
+gridSizeBtn.addEventListener("click", (e) => {
+    let size = prompt("Enter your prefered grid size:", "Max 100");
+
+    if (size <= 100){
+        gridSize = size;
+    } else {
+        alert("Invalid Input");
+    }
+    removePrevGrid();
+    createGrid(gridSize);
+});
+
+function createGrid (gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add('row');
+        grid.appendChild(row);
+        for (let i = 0; i < gridSize; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add('cell');
+            row.appendChild(cell);
+    
+            addHoverEffect(cell);
+        }
     }
 }
 
-
-function addHoverEffect(item) {
+function addHoverEffect (item) {
     item.addEventListener("mouseenter", (e) => {
         e.target.style.background = "black";
     });
+}
+
+function removePrevGrid () {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
 }
